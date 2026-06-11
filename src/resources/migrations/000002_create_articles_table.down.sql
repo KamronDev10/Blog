ee08febb1db0546b68d264426c1022a364e78639
Kamron <kkama3326@gmail.com>
2026-06-11 17:33:10 +0500

    changed table

--- src/resources/migrations/000002_create_articles_table.down.sql
+++ src/resources/migrations/000002_create_articles_table.down.sql
@@ -1,11 +1,2 @@
-CREATE TABLE articles (
-    id         SERIAL PRIMARY KEY,
-    title      VARCHAR(255) NOT NULL,
-    content    TEXT NOT NULL,
-    view_count INT DEFAULT 0,
-    active     BOOLEAN DEFAULT true,
-    created_at TIMESTAMP DEFAULT NOW(),
-    user_id    INT NOT NULL,
 
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS articles;
