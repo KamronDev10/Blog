368440d504be22ecf20327451d735c4d0fe34b0a
Kamron <kkama3326@gmail.com>
2026-06-21 19:52:23 +0500

    w

--- src/main/app/api/api.go
+++ src/main/app/api/api.go
@@ -2,14 +2,25 @@ package api
 
 import (
 	"blog_app/src/main/app/api/handler"
+	"blog_app/src/main/common/middleware"
 	"net/http"
 )
 
 func Api(router *http.ServeMux, handler *handler.Handler) {
-	router.HandleFunc("POST /articles/create", handler.Create)
+	router.Handle(
+		"POST /articles/create",
+		middleware.AuthMiddleware(http.HandlerFunc(handler.Create)))
+
+	router.Handle(
+		"PUT /articles/update",
+		middleware.AuthMiddleware(http.HandlerFunc(handler.Update)))
+
+	router.Handle(
+		"DELETE /articles/delete",
+		middleware.AuthMiddleware(http.HandlerFunc(handler.Delete)))
+
+	// -------------------------------------------------------------  not middle ware
 	router.HandleFunc("GET /articles", handler.GetAll)
-	router.HandleFunc("PUT /articles/update", handler.Update)
-	router.HandleFunc("DELETE /articles/delete", handler.Delete)
 	router.HandleFunc("GET /articles/get", handler.Get)
 
 }
