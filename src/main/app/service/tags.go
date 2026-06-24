6bf48e0b827f5f959dd0f138bc3376305d723853
Kamron <kkama3326@gmail.com>
2026-06-24 20:47:38 +0500

    tags service

---
+++ src/main/app/service/tags.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"blog_app/src/main/app/models"
+	"blog_app/src/main/app/repository"
+)
+
+type TagServiceI interface {
+	CreateTag(tag *models.Tag) error
+	GetAll() ([]*models.Tag, error)
+	GetByID(id int) (*models.Tag, error)
+	Delete(id int) error
+}
+
+type TagService struct {
+	tagrepo repository.TagRepoI
+}
+
+func NewTagService(tagrepo repository.TagRepoI) TagServiceI {
+	return &TagService{tagrepo: tagrepo}
+}
+
+func (ts *TagService) CreateTag(tag *models.Tag) error {
+	return ts.tagrepo.CreateTag(tag)
+}
+
+func (ts *TagService) GetAll() ([]*models.Tag, error) {
+
+	tags, err := ts.tagrepo.GetAll()
+}
